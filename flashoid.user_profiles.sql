ec2e92fdf52ef9b3960b7360628d2e4f69e26905
anasouardini <ouardinianas@proton.me>
2024-08-10 00:05:58 +0100

    only necessary

--- flashoid.user_profiles.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `user_profiles` VALUES
-("9cb9544f-4fb6-44df-9c18-15ea72907545","9cb9544f-4c8f-4362-9a43-6ff50e6f72d1","dark",CONVERT("[\"https://www.flashoid.com/dictionaries/lsdkfjlk234lkjl3k4lk3\"]" USING UTF8MB4),"2024-08-09 09:06:13","2024-08-09 09:06:13");
